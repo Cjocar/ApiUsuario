3b1bdf95b3f591ef1360893b8b9cbd8010db1d35
Cjocar <167594403+Cjocar@users.noreply.github.com>
2025-02-11 21:40:21 -0300

    adicao de metodo para salvar o usuario

---
+++ usuario/src/main/java/com/javaNauta/usuario/business/dto/EnderecoDTO.java
@@ -0,0 +1,26 @@
+package com.javaNauta.usuario.business.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EnderecoDTO {
+
+    private String rua;
+
+    private String numero;
+
+    private String complemento;
+
+    private String cidade;
+
+    private String estado;
+
+    private String cep;
+
+
+
+}
